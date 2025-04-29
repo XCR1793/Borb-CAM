6b716b2314e21d2d83d72f948bc7c72a2f725b73
XCR-1793 <138337647+XCR1793@users.noreply.github.com>
2025-04-29 22:11:23 +1000

    Added Rotation Coefficient Conversion Equations

--- Software/src/slice.cpp
+++ Software/src/slice.cpp
@@ -1,22 +1,37 @@
 #include "slice.h"
 
 /**##########################################
+ * #               Maths Tools              #
+ * ##########################################*/
+
+// Convert Rotation X Y to Coefficient A B C
+Vector3 slice::rotation_coefficient(float Rotation_X, float Rotation_Y){
+    return (Vector3){sin(Rotation_Y), -sin(Rotation_X)*cos(Rotation_Y), cos(Rotation_X)*cos(Rotation_Y)};
+}
+
+
+
+
+/**##########################################
  * #              Slicing Tools             #
  * ##########################################*/
 
-// 
+// Find Model Pathing
+vectors_per_model slice::model_path(Model model){
+    return vectors_per_model{};
+}
 
 
 
 // Function to Slice
-bool slice::Slice(){}
+bool slice::Slice(){};
 
 // Return Sliced Pathing
-vectors_per_model slice::Return_Pathing(int id){
+paths slice::Return_Pathing(int id){
     for(auto p : path_list){
         if(p.id == id){
-            return p.path_list;
+            return p;
         }
     }
-    return vectors_per_model{};
+    return paths{};
 }
--- Software/src/slice.h
+++ Software/src/slice.h
@@ -3,14 +3,10 @@
 
 #include <raylib.h>
 #include <vector>
-#include <meshmanagement.h>
-#include <pathing.h>
+#include "meshmanagement.h"
+#include "pathing.h"
 
-struct path_point{
-    Vector3 position;
-    Vector3 rotation;
-    int point_number;
-};
+struct path_point{Vector3 position; Vector3 rotation; int point_number;};
 
 struct vector_set{std::vector<path_point> point_list;};
 
@@ -18,39 +14,41 @@ struct vectors_per_mesh{std::vector<vector_set> mesh_vector_list; int id;};
 
 struct vectors_per_model{std::vector<vectors_per_mesh> model_vector_list;};
 
-struct path_model_params{
-    Model model;
-    Vector4 slice_plane_equation; // First 3: Coeff X Y Z, Last Digit: slice height
-};
-
 struct paths{
     int id;
     vectors_per_model path_list;
-    path_model_params model_list;
+    Vector4 slice_equation; // First 3: Rotation X Y Z, Last Digit: slice height
 };
 
 
 class slice{
     public:
     /**##########################################
+     * #               Maths Tools              #
+     * ##########################################*/
+
+    // Convert Rotation X Y to Coefficient A B C
+    Vector3 rotation_coefficient(float Rotation_X, float Rotation_Y);
+    
+    /**##########################################
      * #              Slicing Tools             #
      * ##########################################*/
 
     // 
 
-
+    // Find Model Pathing
+    vectors_per_model model_path(Model model);
 
     // Function to Slice
     bool Slice();
 
     // Return Sliced Pathing
-    vectors_per_model Return_Pathing(int id);
-
-
-
+    paths Return_Pathing(int id);
 
     private:
     std::vector<paths> path_list;
+    mesh model_list;
+    path pathing;
 };
 
 #endif
